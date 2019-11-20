9d720ddc803955e81666c03f3609cffeb92e9f0d
Beardocracy <travisjbearden@gmail.com>
2019-11-20 04:34:49 +0000

    Added error message functions

--- errors.c
+++ errors.c
@@ -16,3 +16,31 @@ int perror_permission(char *file, char *caller)
 
 	return (0);
 }
+
+/**
+ * perror_file - prints the no such file or directory error message
+ * @file: the name of the file
+ * @caller: the name of the executable file that made the call
+ * Return: 0
+ */
+int perror_file(char *file, char *caller)
+{
+	
+	_putchar('-');
+	print_string(caller);
+	print_string(": ");
+	print_string(file);
+	print_string(": No such file or directory\n");
+	return (0);
+}
+
+/**
+ * perror_command - prints the command not found error message
+ * @command: the command
+ * Return: 0
+ */
+int perror_command(char *command)
+{
+	print_string(command);
+	print_string(": command not found\n");
+}
