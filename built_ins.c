c1e3a678a10e68dd585d9e4c4a8f8c2995edaf95
Beardocracy <travisjbearden@gmail.com>
2019-11-20 03:38:41 +0000

    Created ptr_array_print function

---
+++ built_ins.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * ptr_array_print - prints an array of strings
+ * @arr: an array of strings containing the variables
+ * Return: the number of strings printed
+ */
+int ptr_array_print(char **arr)
+{
+	int i, j;
+
+	for (i = 0; arr[i]; i++)
+	{
+		print_string(arr[i]);
+		_putchar('\n');
+	}
+
+	return (i - 1);
+}
