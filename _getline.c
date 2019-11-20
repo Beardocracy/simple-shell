46d3e5b23c502ff1616d1c7e9a9c830f4daf2558
Beardocracy <travisjbearden@gmail.com>
2019-11-20 04:48:12 +0000

    Relocated _strcmp function to this file

--- _getline.c
+++ _getline.c
@@ -63,3 +63,17 @@ int _strcmp(char *sea, char *bass)
 
 	return (0);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
