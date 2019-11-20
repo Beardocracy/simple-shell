925f851d51f03727d44f16673a2e35069056c7a0
Beardocracy <travisjbearden@gmail.com>
2019-11-20 02:40:10 +0000

    Created switcher and executer functions

---
+++ value_switcher.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * switcher - directs traffic
+ * @val: integer that tells me where to send other arguments
+ * @com_path: string that contains either a absolute path or other command.
+ * @flags: the rest of what was accepted from getline
+ * @env: a copy of the environment variables
+ * Return: 0 if successful
+ */
+int switcher(int val, char *com_path, char **flags, char **env)
+{
+	switch(val) {
+
+		case 1 :
+			executer(com_path, flags);
+			return (0);
+	}
+	return (1);
+}
+/**
+ * executer - forks current process to run execve
+ * @command - the absolute path of an executable
+ * @flags - an array of strings, including extra input for the command
+ * Return: 0 if successful, -1 if not
+ */
+int executer(char *command, char **flags)
+{
+	pid_t kid_pid;
+	int status;
+
+	kid_pid = fork();
+	if (kid_pid == -1)
+		return (-1);
+	if (kid_pid == 0)
+	{
+		execve(command, flags, NULL);
+		exit(187);
+	}
+	else
+		wait(&status);
+
+	return (0);
+}
