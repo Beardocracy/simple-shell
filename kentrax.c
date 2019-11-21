55e1b8b67a05b29ed99a7cf6328db27e12779ae5
Beardocracy <travisjbearden@gmail.com>
2019-11-21 01:29:59 +0000

    Added ctrl-c capturing

--- kentrax.c
+++ kentrax.c
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <signal.h>
+
+typedef void (*sighandler_t)(int);
 
 /**
  * main - a simple shell
@@ -19,8 +22,11 @@ int main(int ac, char *av[], char *env[])
 	char delim = ' ';
 	ssize_t num_read;
 	size_t n = 0;
-	int i, flag, path_value;
+	int i, flag, path_value, z;
+	sighandler_t ignore;
 
+	if (signal(SIGINT, SIG_IGN) != SIG_IGN)
+	    signal(SIGINT, ignore);
 	while (num_read != -1)
 	{
 		if (isatty(STDIN_FILENO))
@@ -29,16 +35,19 @@ int main(int ac, char *av[], char *env[])
 		if (num_read == -1)
 			break;
 		del_newline(buffer);
-		com[0] = strtok(buffer, &delim);
-		for (i = 1, flag = 1; flag; i++)
+		if (buffer[0])
 		{
-			com[i] = strtok(NULL, &delim);
-			if (com[i] == NULL)
-				flag = 0;
+			com[0] = strtok(buffer, &delim);
+			for (i = 1, flag = 1; flag; i++)
+			{
+				com[i] = strtok(NULL, &delim);
+				if (com[i] == NULL)
+					flag = 0;
+			}
+			com_path = get_path(com[0], env, &path_value);
+			switcher(path_value, com_path, com, env, av);
+			free(com_path);
 		}
-		com_path = get_path(com[0], env, &path_value);
-		switcher(path_value, com_path, com, env, av);
-		free(com_path);
 	}
 	if (buffer)
 		free(buffer);
