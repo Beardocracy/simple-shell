b129223ed0057bf7ea78ae37135e18543616dac5
Beardocracy <travisjbearden@gmail.com>
2019-11-26 21:59:41 +0000

    Added file in function for if file does not exist

--- kentrax.c
+++ kentrax.c
@@ -23,16 +23,22 @@ int main(int ac, char *av[], char *env[])
 	char *buffer = NULL, *com[32], *com_path = NULL;
 	ssize_t num_read = 1;
 	size_t n = 0;
-	int i, flag, path_value, com_count = 0, ret = 0;
+	int i, flag, path_value, com_count = 0, ret = 0, file_in_flag = 1;
 	(void) ac;
 
 	if (signal(SIGINT, SIG_IGN) != SIG_IGN)
 		signal(SIGINT, ignore);
-	while (num_read != EOF)
+	while (num_read != EOF && file_in_flag)
 	{
 		if (isatty(STDIN_FILENO))
 			print_prompt();
-		num_read = getline(&buffer, &n, stdin);
+		if (av[1] != NULL)
+		{
+			buffer = _strdup(av[1]);
+			file_in_flag = 0;
+		}
+		else
+			num_read = getline(&buffer, &n, stdin);
 		if (num_read == EOF)
 		{
 			free(buffer);
